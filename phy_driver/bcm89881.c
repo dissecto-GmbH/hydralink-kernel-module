956af62bae42f25bf717884809ccd85965227e78
Enrico Pozzobon <enrico@dissec.to>
2024-10-18 14:57:02 +0200

    improved bcm89881 compatibility with ethtool

--- phy_driver/bcm89881.c
+++ phy_driver/bcm89881.c
@@ -1,117 +1,75 @@
 #include <linux/ethtool.h>
 #include <linux/module.h>
 #include <linux/phy.h>
+#include <linux/mdio.h>
 
-#define PHY_ID_BCM89881 (0xae025032)
+#define PHY_ID_BCM89881 (0xae025030)
+#define PHY_ID_BCM89881_MASK (0xfffffff0)
 
 static int bcm89881_read_status(struct phy_device *phydev)
 {
 	int ret = 0;
 
-	phydev->link = 1;
-
-	phydev->speed = 1000;
-	phydev->duplex = DUPLEX_UNKNOWN;
+	phydev->duplex = DUPLEX_FULL;
 	phydev->pause = 0;
 	phydev->asym_pause = 0;
 
 	ret = genphy_c45_read_pma(phydev);
+	int pma_pmd_status = phy_read_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_STAT1);
+	int ieee_pma_pmd_baset1_control = phy_read_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_PMA_PMD_BT1_CTRL);
+
+	phydev->speed = (ieee_pma_pmd_baset1_control & MDIO_PMA_PMD_BT1_CTRL_STRAP_B1000) == 0 ? SPEED_100 : SPEED_1000;
+	phydev->link = (pma_pmd_status & BMSR_LSTATUS) ? 1 : 0;
+	phydev->master_slave_state = (ieee_pma_pmd_baset1_control & MDIO_PMA_PMD_BT1_CTRL_CFG_MST) ? MASTER_SLAVE_STATE_MASTER : MASTER_SLAVE_STATE_SLAVE;
 
 	linkmode_set_bit(ETHTOOL_LINK_MODE_MII_BIT, phydev->supported);
 	phydev->port = PORT_MII;
 
-	phydev->master_slave_state = MASTER_SLAVE_STATE_UNKNOWN;
-	phydev->master_slave_set = MASTER_SLAVE_CFG_UNKNOWN;
-
-	return ret;
-}
-
-static int modify(struct phy_device *phydev, int devad, int addr, int set,
-		  int clr)
-{
-	int reg = phy_read_mmd(phydev, devad, addr);
-	int old = reg;
-
-	reg &= (~clr) & 0xffff;
-	reg |= set;
-
-	if (old != reg)
-		phy_write_mmd(phydev, devad, addr, reg);
-
-	return reg;
+	return 0;
 }
 
 static int bcm89881_config_init(struct phy_device *phydev)
 {
 	phydev->autoneg = AUTONEG_DISABLE;
 
-	return 0;
-}
+	// RGMII delay and voltage settings
+	phy_write_mmd(phydev, MDIO_MMD_PMAPMD, 0xa010, 0x0001);
+	phy_write_mmd(phydev, MDIO_MMD_PMAPMD, 0xa015, 0x0000);
 
-static int bcm89881_match_phy_device(struct phy_device *phydev)
-{
-	return phydev->c45_ids.device_ids[1] == PHY_ID_BCM89881;
+	// set leds
+	phy_write_mmd(phydev, MDIO_MMD_PMAPMD, 0xa027, 0x0f15);
+	phy_write_mmd(phydev, MDIO_MMD_PMAPMD, 0x931e, 0x0063);
+	phy_write_mmd(phydev, MDIO_MMD_PMAPMD, 0x931d, 0x0010);
+	phydev_info(phydev, "Initialization done\n");
+
+	// populate configuration for ethtool
+	return genphy_c45_pma_baset1_read_master_slave(phydev);
 }
 
 static int bcm89881_config_aneg(struct phy_device *phydev)
 {
-	modify(phydev, 1, 0x0000, 0x8000, 0x0000);
-
 	if (phydev->autoneg == AUTONEG_DISABLE) {
-		modify(phydev, 1, 0x0000, 0x8000, 0x0000);
-
 		switch (phydev->speed) {
 		case SPEED_100:
-			modify(phydev, 1, 0x0000, 0x2000, 0x0040);
+			phy_modify_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_CTRL1, BMCR_SPEED100 | BMCR_SPEED1000, BMCR_SPEED100);
 			break;
 		case SPEED_1000:
-			modify(phydev, 1, 0x0000, 0x0040, 0x2000);
+			phy_modify_mmd(phydev, MDIO_MMD_PMAPMD, MDIO_CTRL1, BMCR_SPEED1000 | BMCR_SPEED100, BMCR_SPEED1000);
 			break;
 		default:
 			return -EINVAL;
 		}
-
-		switch (phydev->master_slave_set) {
-		case MASTER_SLAVE_CFG_MASTER_PREFERRED:
-		case MASTER_SLAVE_CFG_MASTER_FORCE:
-			modify(phydev, 1, 0x0834, 0x4000, 0x0000);
-			phydev_warn(phydev, "Switched to master mode\n");
-			break;
-		case MASTER_SLAVE_CFG_SLAVE_FORCE:
-		case MASTER_SLAVE_CFG_SLAVE_PREFERRED:
-			modify(phydev, 1, 0x0834, 0x0000, 0x4000);
-			phydev_warn(phydev, "Switched to slave mode\n");
-			break;
-		case MASTER_SLAVE_CFG_UNKNOWN:
-			phydev_warn(phydev, "Unknown Master/Slave mode\n");
-			break;
-		case MASTER_SLAVE_CFG_UNSUPPORTED:
-			phydev_warn(phydev, "Unsupported Master/Slave mode\n");
-			break;
-		default:
-			phydev_err(phydev, "Bad Master/Slave mode\n");
-			return -EOPNOTSUPP;
-		}
-
-		modify(phydev, 1, 0x0000, 0x0000, 0x8000);
-
-		phy_write_mmd(phydev, 1, 0xa010, 0x0001);
-		phy_write_mmd(phydev, 1, 0xa015, 0x0000);
-
-		// set leds
-		phy_write_mmd(phydev, 1, 0xa027, 0x0f15);
-		phy_write_mmd(phydev, 1, 0x931e, 0x0063);
-		phy_write_mmd(phydev, 1, 0x931d, 0x0010);
 	}
-	return 0;
+
+	genphy_c45_pma_baset1_setup_master_slave(phydev);
+	// repopulate configuration for ethtool
+	return genphy_c45_pma_baset1_read_master_slave(phydev);
 }
 
 static struct phy_driver bcm89881_driver[] = { {
-	.phy_id = PHY_ID_BCM89881,
-	.phy_id_mask = 0xfffffff0,
+	PHY_ID_MATCH_MODEL(PHY_ID_BCM89881),
 	.name = "Broadcom BCM89881",
 	.read_status = bcm89881_read_status,
-	.match_phy_device = bcm89881_match_phy_device,
 	.config_aneg = bcm89881_config_aneg,
 	.get_features = genphy_c45_pma_read_abilities,
 	.config_init = bcm89881_config_init,
